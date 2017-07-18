b449e5bbff4a68d0ec8660e0b44f04c85e2a41ed
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-07-18 14:13:47 +0300

    Add project files.

---
+++ Hospital/Models/Patient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Models
+{
+    public enum PatientStatus
+    {
+        Arrived = 1,
+        Sick,
+        Healthy
+    }
+
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 1 - Arrived
+        /// 2 - Sick
+        /// 3 - Healthy
+        /// 
+        /// TODO: change it to enum
+        /// </summary>
+        public PatientStatus Status { get; set; }
+
+        public DateTime DayOfBirth { get; set; }
+
+        /// <summary>
+        /// Идентификационный код.
+        /// </summary>
+        public string TaxCode { get; set; }
+
+        public virtual ICollection<Doctor> Doctors { get; set; }
+    }
+}
